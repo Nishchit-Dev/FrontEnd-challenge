b3ecf0699428dbc1c9c23c41ac82f2ac9ff92426
Nishchit-dev <nishchitpatel84@gmail.com>
2024-02-19 12:02:10 -0600

    added chakra-support

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Center } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Center h="100vh">
+        <Box h="100px" w="100px" bg="blue"></Box>
+      </Center>
+    </>
   );
 }
 
